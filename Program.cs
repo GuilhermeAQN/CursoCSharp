734968d5d0390eeb0d835c19e57838460c4498df
GuilhermeAQN <guilhermeaqn92@gmail.com>
2022-11-28 20:35:26 -0400

    Pequena atividade de placeholder, concatenação e interpolação

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+class Program {
+    static void Main(){
+        string produto1 = "Computador";
+        string produto2 = "Mesa de escritório";
+
+        byte idade = 30;
+        int codigo = 5290;
+        char genero = 'M';
+
+        double preco1 = 2100.0;
+        double preco2 = 650.50;
+        double medida = 53.234567;
+
+
+        Console.WriteLine($"{produto1}, cuja o preço é $ {preco1}\n{produto2}, cuja o preço é $ {preco2}\n");
+        
+        Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e genero {genero}\n");
+
+        Console.WriteLine($"Medida com oito casas decimais: {medida:F8}\nArredondado(três casas decimais): {medida:F3}");
+
+        Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+    }
+}
